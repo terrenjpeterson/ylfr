5ee710d60f62b6fb22812012ae82ddaf28503b60
terrenjpeterson <ubuntu@ip-172-31-12-86.us-west-2.compute.internal>
2014-11-27 17:54:00 +0000

    initial node webserver

---
+++ ylfr.js
@@ -0,0 +1,59 @@
+// include the required node.js packages
+
+var express = require('express');
+var http = require('http');
+var fs = require('fs');
+
+// set variables for each html page served
+
+var INPUT_FILE = "index.html";
+var BETTER_FILE = "bootstrap.html";
+var DONATE_FILE = "donate.html";
+var CONTACT_FILE = "contact.html";
+var ABOUT_FILE = "about.html";
+var AFTER_SCHOOL_FILE = "after_school.html";
+var SUMMER_PROGRAMS_FILE = "summer_program.html";
+var FINANCIAL_LITERACY_FILE = "financial_literacy.html";
+
+// create the server
+
+var app = express();
+
+var server = http.createServer(app);
+
+// load the html pages in memory of the server
+
+var home_page = fs.readFileSync(INPUT_FILE, 'utf8');
+var better_page = fs.readFileSync(BETTER_FILE, 'utf8');
+var donate_page = fs.readFileSync(DONATE_FILE, 'utf8');
+var contact_page = fs.readFileSync(CONTACT_FILE, 'utf8');
+
+// create the routes to handle http requests for individual html pages
+
+app.get('/', function(req, res){
+    res.send(better_page);
+});
+
+app.get('/index.html', function(req, res) {
+   res.send(better_page);
+});
+
+app.get('/terren', function(req, res) {
+    res.send('now you get it');
+});
+
+app.get('/better.html', function(req, res) {
+    res.send(better_page);
+});
+
+app.get('/donate.html', function(req, res) {
+    res.send(donate_page);
+});
+
+app.get('/contact.html', function(req, res) {
+    res.send(contact_page);
+});
+
+// begin listening on the port for traffic
+
+app.listen(8080);
